4edf126c22ff3ae0f423bff434d3024d7660c618
Hussain Ahmad <hussainahmad4658@gmail.com>
2024-03-05 17:23:31 +0500

    new

--- screens/Away.jsx
+++ screens/Away.jsx
@@ -1,22 +1,35 @@
 import { View, Text, Switch, TouchableOpacity } from 'react-native'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default function Away({ navigation }) {
+
+    console.log('Away top area')
 
     const [isEnabled, setIsEnabled] = useState(global.mode)
 
     const toggleSwitch = () => {
+        console.log('Away toggle switch')
 
+        console.log('global.mode before : ', global.mode)
         setIsEnabled((previousState) => !previousState)
-
         global.mode = !global.mode
+        console.log('global.mode after : ', global.mode)        
     }
+
+    useEffect(() => {
+        console.log('Away useEffect\n')
+        return () => {
+            console.log('Away useEffect return\n')
+            // console.log('Going Back');
+        }
+    }, [])
+
     return (
 
-        <View className={`flex-1 bg-[${!global.mode ? '#c3ff83' : '#478db2'}]`}>
-            <View className='flex-[.5] items-center justify-center flex-row '>
+        <View className={`flex-1 ${!global.mode ? 'bg-[#c3ff83]' : 'bg-[#478db2]'}`}>
+            <View className='flex-[.5] items-center justify-center flex-row'>
 
-                <Text className='mr-[50] text-3xl'>Dark Mode {console.log('return test')}</Text>
+                <Text className='mr-[50] text-3xl'>Dark Mode</Text>
                 <Switch
                     trackColor={{ false: '#767577', true: '#81b0ff' }}
                     thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
@@ -28,6 +41,8 @@ export default function Away({ navigation }) {
                 </TouchableOpacity>
 
             </View>
+
+            {console.log('Away return')}
 
             <View className='flex-[.5] items-center justify-center'>
                 <Text className='text-3xl' onPress={() => navigation.navigate('Home')}>Back</Text>
--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,16 +1,35 @@
 import { View, Text, TouchableOpacity } from 'react-native'
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 global.mode = false
 
 export default function Home({navigation}) {
+
+    console.log('Home top area')
+
+    const [mode, setMode] = useState(false)
+
+    useEffect(() => {
+        console.log('Home useEffect top')
+
+        const unsubscribe = navigation.addListener('focus', () => {
+            setMode((previousState) => !previousState)
+            console.log('Home useEffect middle, global.mode : ', global.mode)
+            // console.log('We are Back')
+        })
+        return unsubscribe
+    }, [navigation])
+
     return (
-        <View className={`flex-1 ${!global.mode ? 'bg-white' : 'bg-orange-300'}`}>
-            <View className='flex-[.5] items-center justify-center bg-slate-400'>
+
+        <View className={`flex-1 ${!global.mode ? 'bg-[#c3ff83]' : 'bg-[#478db2]'}`}>
+            <View className='flex-[.5] items-center justify-center'>
                 <Text className='text-3xl'>Home</Text>
             </View>
 
-            <View className='flex-[.5] items-center justify-center bg-slate-200'>
+            {console.log('Home return')}
+
+            <View className='flex-[.5] items-center justify-center'>
                 <TouchableOpacity onPress={() => navigation.navigate('Away')}>
                     <Text className='text-3xl'>Next</Text>
                 </TouchableOpacity>
